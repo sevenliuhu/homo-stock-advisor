7b1dc6d263be7d56357b618cf582745b14c9593d
HOMO_LAU <16208204@qq.com>
2026-05-21 13:33:15 +0800

    add license validator

---
+++ license_validator.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2026 HOMO AI. License required.
+import os; key=os.environ.get('HOMO_LICENSE_KEY',''); __license_ok__ = len(key)>=16
+if not __license_ok__: print('[HOMO] License required. Contact: 16208204@qq.com')
